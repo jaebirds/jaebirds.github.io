73492afd8f741ee72dbbc09674fb0a7c2ca95e1a
jaebirds <julia.cordeiro2@outlook.com>
2017-06-07 19:15:25 -0400

    -

---
+++ hffoods/assets/js/farmers-indiv.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+	$('.farmers-main-carousel').owlCarousel({
+		items: 1,
+		loop: true,
+		nav: false,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		//autoplayHoverPause: true
+	});
+	
+	$('.farmers-indiv-bottom-carousel').owlCarousel({
+		center: true,
+		items: 3,
+		margin: 0,
+		nav: false,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 5000,
+	});
+
+});
