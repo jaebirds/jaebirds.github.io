7fba944bf63506ffe6d05d290bab59bd9ff74377
jaebirds <julia.cordeiro2@outlook.com>
2017-12-15 10:30:53 -0500

    -

--- testing/hffoods/assets/js/firebase-init.js
+++
@@ -1,6 +0,0 @@
-var config = {
-    apiKey: " AIzaSyAGq2lfnEvSluD3FCg3tf-z723kFAuwvQU",
-    authDomain: "hf-foods.firebaseapp.com",
-    databaseURL: "https://hf-foods.firebaseio.com/",
-};
-firebase.initializeApp(config);
